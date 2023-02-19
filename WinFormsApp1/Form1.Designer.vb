3a6abda36fc249e555c4283cee461d6007af4ae4
Rido Mulanda <ridomulanda@gmail.com>
2023-02-18 19:20:34 -0800

    Read, save, save as

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.title = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.filepath = New System.Windows.Forms.TextBox()
+        Me.browse = New System.Windows.Forms.Button()
+        Me.content = New System.Windows.Forms.RichTextBox()
+        Me.save = New System.Windows.Forms.Button()
+        Me.save_as = New System.Windows.Forms.Button()
+        Me.content_csv = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.content_csv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'title
+        '
+        Me.title.AutoSize = True
+        Me.title.Font = New System.Drawing.Font("Segoe UI", 19.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.title.Location = New System.Drawing.Point(12, 9)
+        Me.title.Name = "title"
+        Me.title.Size = New System.Drawing.Size(300, 36)
+        Me.title.TabIndex = 0
+        Me.title.Text = "File Reader: TXT and CSV"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "File :"
+        '
+        'filepath
+        '
+        Me.filepath.Location = New System.Drawing.Point(49, 84)
+        Me.filepath.Name = "filepath"
+        Me.filepath.ReadOnly = True
+        Me.filepath.Size = New System.Drawing.Size(495, 23)
+        Me.filepath.TabIndex = 2
+        '
+        'browse
+        '
+        Me.browse.Location = New System.Drawing.Point(550, 87)
+        Me.browse.Name = "browse"
+        Me.browse.Size = New System.Drawing.Size(75, 23)
+        Me.browse.TabIndex = 3
+        Me.browse.Text = "Browse"
+        Me.browse.UseVisualStyleBackColor = True
+        '
+        'content
+        '
+        Me.content.Location = New System.Drawing.Point(12, 172)
+        Me.content.Name = "content"
+        Me.content.Size = New System.Drawing.Size(776, 290)
+        Me.content.TabIndex = 4
+        Me.content.Text = ""
+        Me.content.Visible = False
+        '
+        'save
+        '
+        Me.save.Location = New System.Drawing.Point(12, 129)
+        Me.save.Name = "save"
+        Me.save.Size = New System.Drawing.Size(134, 23)
+        Me.save.TabIndex = 5
+        Me.save.Text = "Save"
+        Me.save.UseVisualStyleBackColor = True
+        Me.save.Visible = False
+        '
+        'save_as
+        '
+        Me.save_as.Location = New System.Drawing.Point(171, 129)
+        Me.save_as.Name = "save_as"
+        Me.save_as.Size = New System.Drawing.Size(134, 23)
+        Me.save_as.TabIndex = 6
+        Me.save_as.Text = "Save As"
+        Me.save_as.UseVisualStyleBackColor = True
+        Me.save_as.Visible = False
+        '
+        'content_csv
+        '
+        Me.content_csv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.content_csv.Location = New System.Drawing.Point(12, 172)
+        Me.content_csv.Name = "content_csv"
+        Me.content_csv.RowTemplate.Height = 25
+        Me.content_csv.Size = New System.Drawing.Size(776, 290)
+        Me.content_csv.TabIndex = 7
+        Me.content_csv.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(342, 327)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 15)
+        Me.Label1.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 543)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.content_csv)
+        Me.Controls.Add(Me.save_as)
+        Me.Controls.Add(Me.save)
+        Me.Controls.Add(Me.content)
+        Me.Controls.Add(Me.browse)
+        Me.Controls.Add(Me.filepath)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.title)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.content_csv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents title As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents filepath As TextBox
+    Friend WithEvents browse As Button
+    Friend WithEvents content As RichTextBox
+    Friend WithEvents save As Button
+    Friend WithEvents save_as As Button
+    Friend WithEvents content_csv As DataGridView
+    Friend WithEvents Label1 As Label
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,208 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class Form1
+
+    Private openedFilePath As String
+    Private openedFileExtension As String
+    Private Const CSV = ".csv"
+    Private Const TXT = ".txt"
+    Private Const FILE_OPEN_FILTER_TXT = "Text files (*.txt)|*.txt"
+    Private Const FILE_OPEN_FILTER_CSV = "CSV files (*.csv)|*.csv"
+
+    Private Sub browseFile(sender As Object, e As EventArgs) Handles browse.Click
+        ' Open a file dialog to let the user select a file
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = FILE_OPEN_FILTER_TXT + "|" + FILE_OPEN_FILTER_CSV
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Read the contents of the file into a string
+            Dim fileContents As String = System.IO.File.ReadAllText(openFileDialog.FileName)
+            openedFileExtension = Path.GetExtension(openFileDialog.FileName)
+            If (openedFileExtension = TXT) Then
+                ' Display the contents of the file in the text editor
+                content.Text = fileContents
+                filepath.Text = openFileDialog.FileName
+
+                ' Store the path of the opened file
+                openedFilePath = openFileDialog.FileName
+                ' Show rich text for place content txt
+                content.Visible = True
+                ' Hide csv datagrid
+                content_csv.Visible = False
+                ' Show save button
+                save.Visible = True
+                ' Show save as button
+                save_as.Visible = True
+            ElseIf (openedFileExtension = CSV) Then
+                ' Declare new datatable when open csv file
+                Dim dt As New DataTable()
+
+                ' Parse data from csv and set delimited comma
+                Using parser As New TextFieldParser(openFileDialog.FileName)
+                    parser.TextFieldType = FieldType.Delimited
+                    parser.SetDelimiters(",")
+                    parser.HasFieldsEnclosedInQuotes = True
+
+                    ' Read the first line to get the column names
+                    Dim columnNames As String() = parser.ReadFields()
+
+                    ' Add the columns to the DataTable
+                    For Each columnName In columnNames
+                        dt.Columns.Add(columnName)
+                    Next
+
+                    ' Loop through the remaining lines of the CSV file
+                    While Not parser.EndOfData
+                        Dim fields As String() = parser.ReadFields()
+
+                        ' Check if the number of fields in the current row exceeds the number of columns in the DataTable
+                        If fields.Length > dt.Columns.Count Then
+                            ' Add new columns to the DataTable
+                            For i As Integer = dt.Columns.Count To fields.Length - 1
+                                dt.Columns.Add("Column " & (i + 1))
+                            Next
+                        End If
+
+                        ' Create a new DataRow and add it to the DataTable
+                        Dim newRow As DataRow = dt.NewRow()
+
+                        ' Loop through the fields in the current line and add them to the DataRow
+                        For i As Integer = 0 To fields.Length - 1
+                            newRow(i) = fields(i)
+                        Next
+
+                        ' Add row to datatable
+                        dt.Rows.Add(newRow)
+                    End While
+                End Using
+
+                ' Display the DataTable in a DataGridView
+                content_csv.DataSource = dt
+
+                filepath.Text = openFileDialog.FileName
+                ' Store the path of the opened file
+                openedFilePath = openFileDialog.FileName
+                ' Hide richtext txt when open csv file
+                content.Visible = False
+                ' Show datagridview 
+                content_csv.Visible = True
+                ' Show save button
+                save.Visible = True
+                ' Shiw save as button
+                save_as.Visible = True
+            End If
+
+        End If
+    End Sub
+
+    Private Sub saveFile(sender As Object, e As EventArgs) Handles save.Click
+
+        ' If opened file extenstion is TXT .txt
+        If (openedFileExtension = TXT) Then
+            ' Save changes to the previously opened file
+            System.IO.File.WriteAllText(openedFilePath, content.Text)
+            ' Show message box after save
+            MessageBox.Show("File has been saved at " + openedFilePath)
+
+            ' If opened file extestion is CSV .csv
+        ElseIf (openedFileExtension = CSV) Then
+            ' Save csv file to opened csv file if returrn true show message box 
+            If (saveCsv(openedFilePath)) Then
+                MessageBox.Show("File has been saved at " + openedFilePath)
+            Else
+                ' Show message box failed
+                MessageBox.Show("Failed saved file")
+            End If
+
+        End If
+    End Sub
+
+    Private Sub saveAs(sender As Object, e As EventArgs) Handles save_as.Click
+        ' Open save dialog and filter by opened file extension
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = If(openedFileExtension = TXT, FILE_OPEN_FILTER_TXT, FILE_OPEN_FILTER_CSV)
+
+        ' Check dialog show
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Check extension openedfile
+            If (openedFileExtension = TXT) Then
+                ' Write the contents of the text box control to the selected file
+                System.IO.File.WriteAllText(saveFileDialog.FileName, content.Text)
+                ' Update the label with the file path
+                filepath.Text = saveFileDialog.FileName
+                ' Store the path of the saved file
+                openedFilePath = saveFileDialog.FileName
+                ' Get file extension from save dialog
+                openedFileExtension = Path.GetExtension(saveFileDialog.FileName)
+                ' Show message box
+                MessageBox.Show("File has been saved at " + openedFilePath)
+            ElseIf (openedFileExtension = CSV) Then
+                If (saveCsv(saveFileDialog.FileName)) Then
+                    ' Update the label with the file path
+                    filepath.Text = saveFileDialog.FileName
+                    ' Store the path of the saved file
+                    openedFilePath = saveFileDialog.FileName
+                    openedFileExtension = Path.GetExtension(saveFileDialog.FileName)
+                    MessageBox.Show("File has been saved at " + saveFileDialog.FileName)
+                Else
+                    MessageBox.Show("Failed saved file")
+                End If
+            End If
+
+        End If
+    End Sub
+
+    Private Function saveCsv(filePath As String) As Boolean
+
+        ' Create a StreamWriter to write the updated data to a new file
+        Dim writer As New StreamWriter(filePath)
+
+        ' Write the column headers to the new file
+        Dim column_list = DirectCast(content_csv.Columns, IList)
+
+        For i = 0 To column_list.Count - 1
+            Dim column As DataGridViewColumn = DirectCast(column_list(i), DataGridViewColumn)
+            If (Not String.IsNullOrEmpty(column.HeaderText)) Then
+                writer.Write(column.HeaderText)
+                If (Not i = column_list.Count - 1) Then
+                    writer.Write(",")
+                End If
+            End If
+
+        Next
+        ' Write new line
+        writer.WriteLine()
+
+        ' Write the data rows to the new file
+        Dim rowList = DirectCast(content_csv.Rows, IList)
+
+        For i = 0 To rowList.Count - 1
+            Dim row As DataGridViewRow = DirectCast(rowList(i), DataGridViewRow)
+            Dim cellList = DirectCast(row.Cells, IList)
+            Dim rowVal = ""
+            For j = 0 To cellList.Count - 1
+                Dim cell As DataGridViewCell = DirectCast(cellList(j), DataGridViewCell)
+                rowVal += cell.Value?.ToString()
+                If (Not j = cellList.Count - 1) Then
+                    rowVal += ","
+                End If
+            Next
+
+            ' Check all column before write to file : if all is empty skip prevent increment row every save
+            If (Not String.IsNullOrEmpty(Replace(rowVal, ",", ""))) Then
+
+                writer.Write(rowVal)
+
+                If (Not i = rowList.Count - 1) Then
+                    writer.WriteLine()
+                End If
+            End If
+
+        Next
+
+        ' Close the StreamWriter
+        writer.Close()
+
+        Return True
+    End Function
+End Class
